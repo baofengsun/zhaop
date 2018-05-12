0e35474ce0d8ffbd5bb0d67578b43fbe27cbeb67
Bao Feng Sun <baofengsun@hotmail.com>
2018-05-12 17:39:36 +0800

    first commit

---
+++ bbs/app/config/myconfig.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array (
+  'site_name' => '航运系统微社区',
+  'index_page' => 'index.php',
+  'show_captcha' => 'on',
+  'site_close' => 'on',
+  'site_close_msg' => '网站升级中，暂时关闭。',
+  'basic_folder' => '',
+  'version' => false,
+  'static' => 'white',
+  'themes' => 'default',
+  'logo' => '<span class=\'green\'>航运BBS</span>',
+  'auto_tag' => 'on',
+  'encryption_key' => '7caa5e5844b846f0066e7beada42bf3f',
+);
---
+++ bbs/app/config/version.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array (
+  'sys_name' => '航运BBS',
+  'sys_url' => 'http://www.yifanyuanhangbbs.com',
+  'sys_version' => 'V1.1',
+  'sys_update' => '20171023',
+  'sys_author' => 'QQ88888888',
+);
